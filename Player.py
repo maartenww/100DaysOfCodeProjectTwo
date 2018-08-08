14a3f390ce58e0901aed342c289dba43b6ccf2fd
Maarten <jachtschipwong@hotmail.com>
2018-08-08 13:21:15 +0200

    Added alien class

--- Player.py
+++ Player.py
@@ -1,14 +1,26 @@
 import pygame as pg
 from settings import *
-# todo: import a new settings file and set starting position (x_pos to the center of the screen
 # todo check http://programarcadegames.com/index.php?chapter=bitmapped_graphics_and_sound (It's a good site)
 class Player:
     x_pos = SCREEN_WIDTH / 2
     y_pos = 0
     player_pos = (x_pos, y_pos)
     player_sprite_width = 42
-    player_sprite_heigth = 46
-    player_sprite_size = (player_sprite_width, player_sprite_heigth)
+    player_sprite_height = 46
+    player_sprite_size = (player_sprite_width, player_sprite_height)
     player_Rect = pg.Rect((player_pos),(player_sprite_size))
-    def hello(self):
-        pass
+    def __init__(self, x_pos=SCREEN_WIDTH/2, y_pos=0):
+        self.y_pos = y_pos
+        self.x_pos = x_pos
+
+class Alien:
+    x_pos = 0
+    y_pos = 0
+    alien_pos = (x_pos, y_pos)
+    alien_sprite_width = 32
+    alien_sprite_height = 62
+    alien_sprite_size = (alien_sprite_width, alien_sprite_height)
+    alien_Rect = pg.Rect((alien_pos), (alien_sprite_size))
+    def __init__(self, x_pos=0, y_pos=0):
+        self.x_pos = x_pos
+        self.y_pos = y_pos
